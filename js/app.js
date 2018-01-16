e80fc0c791a6ce0c58ea373f5160b944730be23f
llixli <igor.belykh@protonmail.com>
2018-01-16 16:42:44 +0300

    work with state: done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,37 @@
 var my_news = [
-	
+  {
+    author: 'Саша Печкин',
+    text: 'В четчерг, четвертого числа...',
+    bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
+  },
+  {
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!',
+    bigText: 'А евро 42!'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+    bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
+  }
 ];
 
 
 var News = React.createClass({
+	propTypes: {
+		data: React.PropTypes.array.isRequired
+	},
+
+	getInitialState: function() {
+		return {
+			counter: 0
+		}
+	},
+
+	onTotalNewsClick: function() {
+		this.setState({counter: ++this.state.counter });
+	},
+
 	render: function() {
 		var data = this.props.data;
 		var newsTemplate;
@@ -12,8 +40,7 @@ var News = React.createClass({
 			newsTemplate = data.map(function(item, index) {
 				return (
 					<div key={index}>
-						<p className="news__author">{item.author}:</p>
-						<p className="news__text">{item.text}</p>
+						<Article data={item} />
 					</div>
 				)
 			})
@@ -25,29 +52,69 @@ var News = React.createClass({
 		return (
 			<div className="news">
 				{newsTemplate}
-				<strong className={data.length > 0 ? '':'none'}>Number of News: {data.length}</strong>
+				<strong 
+					className={'news__count ' + (data.length > 0 ? '':'none')}
+					onClick={this.onTotalNewsClick}>
+					Number of News: {data.length}
+				</strong>
 			</div>
 		);
 	}
 });
 
-var Comments = React.createClass({
+
+var Article = React.createClass({
+	propTypes: {
+		data: React.PropTypes.shape({
+			author: React.PropTypes.string.isRequired,
+			text: React.PropTypes.string.isRequired,
+			bigText: React.PropTypes.string.isRequired
+		})
+	},
+
+	getInitialState: function() {
+		return {
+			visible: false
+		};
+	},
+
+	readmoreClick: function(e) {
+		e.preventDefault();
+		this.setState({visible:true});
+	},
+
 	render: function() {
+		var author = this.props.data.author,
+		text = this.props.data.text,
+		bigText = this.props.data.bigText,
+		visible = this.state.visible;
+
+		console.log('render', this);
+
 		return (
-			<div className="comments">
-				No news - no comments
-			</div>
-		);
+			<div className='article'>
+				<p className='news__author'>{author}:</p>
+				<p className='news__text'>{text}</p>
+
+				<a href="#" 
+					onClick={this.readmoreClick} 
+					className={'news_readmore ' + (visible ? 'none': '')}>
+					Подробнее
+				</a>
+				
+				<p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
+			</div>	
+		)
 	}
- });
+});
+
 
 var App = React.createClass({
 	render: function() {
 		return (
 			<div className="App">
-				Hi everybody, I'm App component! I can display News.
-				<News data={my_news} /> {/*add option data */}
-				<Comments />
+				<h3>News</h3>
+				<News data={my_news}/> {/*add option data */}
 			</div>
 		);
 	}
